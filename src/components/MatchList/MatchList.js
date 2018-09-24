e6bf982e1ff647ba3da9f1cc2e7a2d759ea155b3
BjarkeHou <bjarke.hou@alexandra.dk>
2018-09-24 11:22:42 +0200

    commit before format

---
+++ src/components/MatchList/MatchList.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import './MatchList.css';
+
+import Player from '../Match/Match.js';
+
+
+class MatchList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            matches: []
+        };
+    }
+
+    componentDidMount() {
+        fetch("http://localhost:8080/matches")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        matches: result
+                    });
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
+
+    render() {
+        const { error, isLoaded, matches } = this.state;
+        if (error) {
+            return <div> Error: { error.message } </div>;
+        } else if (!isLoaded) {
+            return <div> Loading... </div>;
+        } else {
+            return (
+                <ul className="matchlist"> {matches.map(match => (
+                    <li className="matchlist_li"> <Match match={match} /></li>
+                    ))}
+                </ul>
+            );
+        }
+    }
+}
+
+export default MatchList;
